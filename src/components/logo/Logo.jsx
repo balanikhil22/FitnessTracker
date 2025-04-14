bad8082b2c6e42557fcc8dd97bc50ec2fddae52b
balanikhil22 <84896546+balanikhil22@users.noreply.github.com>
2025-04-14 23:57:33 +0530

    Workout Recommendation

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,9 +1,13 @@
-import LogoImg from '../../assets/logo.png';
+import LogoImg from "../../assets/logo.png";
 
 const Logo = () => {
   return (
     <div className="flex items-center space-x-2">
-      <img className="logo" src={LogoImg}></img>
+      <img
+        className="logo"
+        src={LogoImg}
+        style={{ height: "100px", width: "100px" }}
+      ></img>
     </div>
   );
 };
